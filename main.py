a4efd5e752680913afa343357e51987b46cccea0
JFlem <jordane.fleming.7@gmail.com>
2025-10-03 17:41:52 -0700

    create main method to test once methods are done

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 def classical(a1, a2):
-    return 
+    return -1
 
 def naive(a1, a2):
-    return
+    return -1
 
 def strassen(a1, a2):
-    return
+    return -1
+
+def main():
+    a1 = [[2,0,-1,6],
+          [3,7,8,0],
+          [-5,1,6,-2],
+          [8,0,1,7]]
+    
+    a2 = [[0,1,6,3],
+          [-2,8,7,1],
+          [2,0,-1,0],
+          [9,1,6,-2]]
+    
+    res = classical(a1,a2)
+    print("Classical Matrix Multipication Result:")
+    print(res)
+    
+    res = naive(a1,a2)
+    print("Naive Divide-and-Conquer Matrix Multiplication Result:")
+    print(res)
+
+    res = strassen(a1, a2)
+    print("Strassen's Matrix Multiplication Result:")
+    print(res)
+
+if __name__ == "__main__":
+    main()
